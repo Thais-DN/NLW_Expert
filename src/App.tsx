916fff4ec15475e51985ac858dc3165e755fcd56
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-06 15:18:54 -0300

    reutilizando components add funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,12 @@ export function App() {
 
             <div className="grid grid-cols-3  gap-6 auto-rows-[250px]">
                 <NewNoteCard />
-                <NodeCard />
-                <NodeCard />
-                <NodeCard />
+                <NodeCard
+                    note={{
+                        date: new Date(),
+                        content: "Hello World",
+                    }}
+                />
             </div>
         </div>
     );
