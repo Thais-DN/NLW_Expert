035aeaed131b2c21e2621d203f2ad6b2d07589d9
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-06 10:21:59 -0300

    curso pt.1

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import logo from "./assets/Logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/New-note-card";
+import { NodeCard } from "./components/Note-card";
+
+export function App() {
+    return (
+        <div className="mx-auto max-w-6xl my-12 space-y-6">
+            <img src={logo} alt="NLW Expert" className="w-32" />
+            <form className="w-full">
+                <input
+                    type="text"
+                    placeholder="Busque em suas notas.."
+                    className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
+                />
+            </form>
+            <div className="h-px bg-slate-700" />
+
+            <div className="grid grid-cols-3  gap-6 auto-rows-[250px]">
+                <NewNoteCard />
+                <NodeCard />
+                <NodeCard />
+                <NodeCard />
+            </div>
+        </div>
+    );
+}
